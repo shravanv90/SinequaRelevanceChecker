9bdff6b530473d2d0a43be8acc18aadba8584919
shravan-lrm <shravan.vishwanathan@leftrightmind.com>
2023-07-04 11:54:49 +0530

    config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 export class Config {
-  app_name: string = 'PharmaApp';
-  query_webservice_name: string = 'pharma_query';
-  username: string = 'shravan';
-  password: string = 'Shravan1123';
-  api_url: string = 'http://72.249.144.188/api/v1/';
+  app_name: string = '<app name>';
+  query_webservice_name: string = '<queryname>';
+  username: string = '<user>';
+  password: string = '<passw>';
+  api_url: string = 'http://<hostname>/api/v1/';
 }
