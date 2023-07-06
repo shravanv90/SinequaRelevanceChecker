fa7ea501c6c90f24f89d733198e3916120cc3b95
shravan-lrm <shravan.vishwanathan@leftrightmind.com>
2023-07-06 22:03:40 +0530

    custom query changes

--- src/app/customquery.service.ts
+++ src/app/customquery.service.ts
@@ -9,25 +9,39 @@ import { Config } from './config';
 export class CustomqueryService {
   constructor(private http: HttpClient, private config: Config) {}
 
+  getPartNames(queryText: string): Observable<any> {
+    const apiUrl = this.config.dataset_url;
+    const requestBody = {
+      user: this.config.username,
+      password: this.config.password,
+      parameters: {
+        param1: queryText,
+      },
+    };
+
+    return this.http.post(apiUrl, requestBody);
+  }
+
   postQuery(
     querytext: string,
     fraction: number,
     partName: string,
     value: number,
-    operator: string
+    operator: string,
+    partNameWeights: { partName: string; partWeight: string }[],
+    relevanceTransformsPayload: string
   ): Observable<any> {
-    const relevanceTransforms = `
-      <RelevanceTransforms>
-        <PartNameBoost fraction="${fraction}">
-          <PartNames mode="and">
-            <PartName>${partName}</PartName>
-          </PartNames>
-          <Action op="${operator}" value="${value}"></Action>
-        </PartNameBoost>
-      </RelevanceTransforms>
-    `;
+    let textPartWeights = '';
+    for (const row of partNameWeights) {
+      if (row.partName && row.partWeight) {
+        textPartWeights += `${row.partName}:${row.partWeight},`;
+      }
+    }
+    if (textPartWeights) {
+      textPartWeights = textPartWeights.slice(0, -1); // Remove the trailing comma
+    }
 
-    const requestBody = {
+    return this.http.post<any>(this.config.api_url + 'search.query', {
       app: this.config.app_name,
       query: {
         name: this.config.query_webservice_name,
@@ -35,16 +49,11 @@ export class CustomqueryService {
         text: querytext,
         page: 1,
         pageSize: 10,
-        relevanceTransforms: relevanceTransforms,
+        relevanceTransforms: relevanceTransformsPayload,
+        textPartWeights,
       },
       user: this.config.username,
       password: this.config.password,
-    };
-
-    console.log('inside post query service');
-    return this.http.post<any>(
-      this.config.api_url + 'search.query',
-      requestBody
-    );
+    });
   }
 }
--- src/app/relevance-checker/relevance-checker.component.ts
+++ src/app/relevance-checker/relevance-checker.component.ts
@@ -11,30 +11,120 @@ export class RelevanceCheckerComponent {
   fraction: number = 0;
   partName: string = '';
   value: number = 0;
-  operator: string = '';
-  partWeight: string = '';
+  operator: string = 'and';
+  partNameWeights: { partName: string; partWeight: string }[] = [];
+  relevanceTransforms: {
+    transform: string;
+    querytext: string;
+    fraction: number;
+    partName: string;
+    value: number;
+    operator: string;
+    partNames: string[];
+  }[] = [];
+
   records: any[] = [];
-  // partNameWeights: { partName: string; partWeight: string }[] = [
-  //   { partName: '', partWeight: '' },
-  // ];
 
   constructor(private customQueryService: CustomqueryService) {}
 
   sendPostRequest() {
+    const relevanceTransformsPayload =
+      this.generateRelevanceTransformsPayload();
+    console.log('Relevance Transforms:', this.relevanceTransforms);
+    console.log('Relevance Transforms Payload:', relevanceTransformsPayload);
+
     this.customQueryService
       .postQuery(
         this.querytext,
         this.fraction,
         this.partName,
         this.value,
-        this.operator
+        this.operator,
+        this.partNameWeights,
+        relevanceTransformsPayload
       )
       .subscribe((data: any) => {
         this.records = data.records;
       });
   }
 
-  // addRow() {
-  //   this.partNameWeights.push({ partName: '', partWeight: '' });
-  // }
+  generateRelevanceTransformsPayload(): string {
+    const relevanceTransformsXml = this.relevanceTransforms
+      .map((transform) => {
+        const selectedPartName = transform.partName; // Get the selected partName
+
+        if (!selectedPartName) return ''; // Skip if no partName is selected
+
+        const partNamesXml = `
+          <${transform.transform} fraction="${transform.fraction}">
+            <PartNames mode="and">
+              ${
+                selectedPartName
+                  ? `<PartName>${selectedPartName}</PartName>`
+                  : ''
+              }
+            </PartNames>
+            <Action op="${transform.operator}" value="${
+          transform.value
+        }"></Action>
+          </${transform.transform}>`;
+
+        return partNamesXml;
+      })
+      .join('\n');
+
+    const relevanceTransformsPayload = `<RelevanceTransforms>
+  ${relevanceTransformsXml}
+</RelevanceTransforms>`;
+
+    return relevanceTransformsPayload;
+  }
+
+  addRelevanceTransform() {
+    const newTransformRow = {
+      transform: 'PartNameBoost',
+      querytext: this.querytext, // Pass the querytext value
+      fraction: 0,
+      partName: '',
+      value: 0,
+      operator: 'and',
+      partNames: [],
+    };
+
+    this.relevanceTransforms.push(newTransformRow);
+
+    // Call getPartNames() to populate part names for the newly added row
+    this.getPartNames(newTransformRow.querytext, newTransformRow);
+  }
+  removeRelevanceTransform(index: number) {
+    this.relevanceTransforms.splice(index, 1);
+  }
+
+  getPartNames(queryText: string, transformRow: any) {
+    if (transformRow.partName === 'text') {
+      transformRow.partNames = ['text'];
+    } else {
+      this.customQueryService
+        .getPartNames(queryText)
+        .subscribe((response: any) => {
+          const partNames =
+            response?.datasets?.partnames?.aggregations[0]?.items.map(
+              (item: any) => item.value
+            );
+          transformRow.partNames = partNames;
+        });
+    }
+  }
+
+  addRow() {
+    const newRow = { partName: '', partWeight: '', partNames: [] };
+    if (this.querytext) {
+      this.getPartNames(this.querytext, newRow);
+    }
+    this.partNameWeights.push(newRow);
+  }
+
+  removeRow(index: number) {
+    this.partNameWeights.splice(index, 1);
+  }
 }
